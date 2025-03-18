0167c6859e6e3710e7ddbc135e6c168a0fb8bcec
Devesh Raghuvanshi <devesh12200@gmail.com>
2025-03-18 20:15:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
       <div className="main">
         <Routes>
-          <Route path="/" element={<Start />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Main />} />
